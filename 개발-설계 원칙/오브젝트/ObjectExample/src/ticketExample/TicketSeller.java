5919e5963d4d6601b04464e81f96862fb1d55516
seongwon <seongwon1290@dsm.hs.kr>
2023-09-21 16:11:58 +0900

    ♻️ :: 1-1. 자율성을 높이자
    Theater는 TicketSeller의 인터페이스에 의존한다

--- 개발-설계 원칙/오브젝트/ObjectExample/src/ticketExample/TicketSeller.java
+++ 개발-설계 원칙/오브젝트/ObjectExample/src/ticketExample/TicketSeller.java
@@ -7,7 +7,15 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }

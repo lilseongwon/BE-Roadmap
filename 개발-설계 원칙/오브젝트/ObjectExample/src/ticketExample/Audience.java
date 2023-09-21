5a973c278abaff800aa66598f09353d90d919459
seongwon <seongwon1290@dsm.hs.kr>
2023-09-21 21:05:39 +0900

    ♻️ :: 1-2. 자율성을 높이자(Audience)

--- 개발-설계 원칙/오브젝트/ObjectExample/src/ticketExample/Audience.java
+++ 개발-설계 원칙/오브젝트/ObjectExample/src/ticketExample/Audience.java
@@ -10,4 +10,15 @@ public class Audience {
     public Bag getBag() {
         return bag;
     }
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
